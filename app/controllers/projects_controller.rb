cc4aa016e76fbaa7a06287003949f6f0083878eb
FatherofDharma <fatherofdharma@gmail.com>
2019-08-09 13:43:07 -0700

    add beginning of controller methods for projects.

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,29 @@
+class ProjectsController< ApplicationController
+  def index
+    
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end

b8b522f3fb7f9e1bedf78369b25223e940583111
FatherofDharma <fatherofdharma@gmail.com>
2019-08-09 13:58:33 -0700

    add basic methods to projects controller.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,29 +1,50 @@
 class ProjectsController< ApplicationController
   def index
-    
+    @projects = Project.all
+    render :index
   end
 
   def new
-
+    @project = Project.new
+    render :new
   end
 
   def create
-
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @project = Project.find(params[:id])
+    render :edit
   end
 
   def show
-
+    @project = Project.find(params[:id])
+    render :show
   end
 
   def update
-
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to projects_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
   end
+
+  private
+    def project_params
+      params.require(:project).permit(:name, :cost, :country_of_origin)
+    end
 end

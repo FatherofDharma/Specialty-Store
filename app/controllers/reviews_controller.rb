23533bdf9172ba14a41fdd0cb3420f84023586a3
FatherofDharma <fatherofdharma@gmail.com>
2019-08-10 20:55:54 -0700

    add code to controller and views to fix adding new review, edit and delete review.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,12 +16,31 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
+    else
+    render :edit
+    end
+  end
+
   def show
     @product = Product.find(params[:product_id])
     @review = Review.find(params[:id])
     render :show
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
   end
 
   private

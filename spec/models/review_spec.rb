f0089fbaed7759a72e82cae921f5e74ff55656ae
FatherofDharma <fatherofdharma@gmail.com>
2019-08-14 20:40:48 -0700

    add vaidations and passing specs for Review.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,5 +3,8 @@ require 'rails_helper'
 describe Review do
 
   it { should belong_to (:product) }
+  it { should validate_presence_of :author }
+  it { should validate_presence_of :content_body }
+  it { should validate_presence_of :rating }
 
 end

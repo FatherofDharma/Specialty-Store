3a6de43849a51d3b6b1afbfe6787809c05e31c65
FatherofDharma <fatherofdharma@gmail.com>
2019-08-14 21:04:00 -0700

    add validations and passing specs for the rating range.

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,6 @@ class Review < ApplicationRecord
   validates :author, presence: true
   validates :content_body, presence: true
   validates :rating, presence: true
+  validates_numericality_of :rating, greater_than: 0
+  validates_numericality_of :rating, less_than: 5
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,5 +6,7 @@ describe Review do
   it { should validate_presence_of :author }
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating }
+  it { should validate_numericality_of(:rating).is_greater_than(0) }
+  it { should validate_numericality_of(:rating).is_less_than(5) }
 
 end

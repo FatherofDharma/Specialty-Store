13349093bd762ffdc0297e3633e2a37b42063cdc
FatherofDharma <fatherofdharma@gmail.com>
2019-08-16 14:31:05 -0700

    add scope for most reviewed products.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,9 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   scope :three_most_recent, -> {order(created_at: :desc).limit(3)}
+  scope :most_reviews, -> {(
+    select("products.id, products.name, products.cost, products.country_of_origin, count(reviews.id) as reviews_count").joins(:reviews).group("products.id").order("reviews_count DESC").limit(10)
+    )}
 
   validates :name, presence: true
   validates :cost, presence: true

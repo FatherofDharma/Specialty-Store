ca781d2e4b5f8d31ccadeb225475b851fa6791a1
FatherofDharma <fatherofdharma@gmail.com>
2019-08-16 15:08:17 -0700

    add a product scope for being made in the usa.

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,8 @@ class Product < ApplicationRecord
     select("products.id, products.name, products.cost, products.country_of_origin, count(reviews.id) as reviews_count").joins(:reviews).group("products.id").order("reviews_count DESC").limit(10)
     )}
 
+  scope :made_in_usa, -> { where("country_of_origin LIKE ?", "%USA%")}
+
   validates :name, presence: true
   validates :cost, presence: true
   validates :country_of_origin, presence: true

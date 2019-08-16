a24363b73fceeff6f0a061acbeb0c7c9645b154c
FatherofDharma <fatherofdharma@gmail.com>
2019-08-16 10:40:57 -0700

    add review min and max validations as well as passing spec.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,8 @@ class Review < ApplicationRecord
 
   validates :author, presence: true
   validates :content_body, presence: true
+  validates :content_body, length: {minimum: 50}
+  validates :content_body, length: {maximum: 200}
   validates :rating, presence: true
   validates_numericality_of :rating, greater_than: 0
   validates_numericality_of :rating, less_than: 5
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,6 +5,7 @@ describe Review do
   it { should belong_to (:product) }
   it { should validate_presence_of :author }
   it { should validate_presence_of :content_body }
+  it { should validate_length_of :content_body }
   it { should validate_presence_of :rating }
   it { should validate_numericality_of(:rating).is_greater_than(0) }
   it { should validate_numericality_of(:rating).is_less_than(5) }

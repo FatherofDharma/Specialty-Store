a898245c2ee40c01aabc5e91be4aa2cb0dfed990
FatherofDharma <fatherofdharma@gmail.com>
2019-08-16 08:41:54 -0700

    add method and passing spec to titleize new product names upon creation.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,11 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :cost, presence: true
   validates :country_of_origin, presence: true
+
+  before_save(:titleize_product)
+
+  private
+    def titleize_product
+      self.name = self.name.titleize
+    end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,4 +8,9 @@ describe Product do
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
 
+
+  it ("titleizes the product name") do
+    product = Product.create(:name => "buck knife", :cost => 35, :country_of_origin => "USA")
+    expect(product.name).to(eq("Buck Knife"))
+  end
 end
